ccf02a5dc7ca064e85bf37bafa517427cc05cbaf
joaorpsoares <joao_rp_soares@hotmail.com>
2015-05-30 21:42:55 +0100

    ...

--- src/astar/Node.java
+++ src/astar/Node.java
@@ -1,5 +1,22 @@
 package astar;
 
-public class Node {
+public class Node implements Comparable<Node> {
+	
+	private String name;
+	private int visitingTime;
+	private int importance;
+	protected boolean visited;
+	protected Node parent;
+	protected double x;
+	protected double y;
+	private float fCost;//H
+	private float gCost;//G
+	private float timeSpent;
+
+	@Override
+	public int compareTo(Node arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
