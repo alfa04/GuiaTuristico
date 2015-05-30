ea410dc7f7168db889d72fd397cc07671035b726
joaorpsoares <joao_rp_soares@hotmail.com>
2015-05-31 00:24:12 +0100

    all ends tomorrow...

--- src/astar/CityBuild.java
+++ src/astar/CityBuild.java
@@ -15,17 +15,20 @@ public class CityBuild {
 		
 	}
 	
-	private static void cityBuild(ArrayList<Node> mapList){
-		File file = new File("resources/latlongDB.txt");
-		for(Node n : mapList){
+	protected void cityBuild(ArrayList<Node> mapList){
+		File file = new File("mapCity/mapCity1.txt");
+		for(Node node : mapList){
 			try(BufferedReader reader = new BufferedReader(new FileReader(file))){
 				for(String line; (line = reader.readLine()) != null; ){
 					String info[] = line.split(Pattern.quote(";"));
-					if((info[0].trim()).equals(n.getName())){
+					if((info[0].trim()).equals(node.getName())){
 						float x = Float.parseFloat(info[1].trim());
 						float y = Float.parseFloat(info[2].trim());
-						n.setX(x);
-						n.setY(y);
+						System.out.print(info[0].trim());
+						System.out.print(Integer.parseInt(info[1].trim()));
+						System.out.println(Integer.parseInt(info[2].trim()));
+						node.setX(x);
+						node.setY(y);
 						break;
 					}
 				}
--- src/astar/main.java
+++ src/astar/main.java
@@ -1,5 +1,24 @@
 package astar;
 
-public class main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+	
+	protected static Graph graph;
+	protected static CityBuild city;
+	AStar astar;
+	
+	
+	
+	public static void main (String[] arg) throws FileNotFoundException, IOException {
+		
+		File file = new File("mapCity/city1.txt");
+		graph = new Graph(file);
+		city = new CityBuild();
+		city.cityBuild(graph.getNodes());
+		
+	}
 
 }
